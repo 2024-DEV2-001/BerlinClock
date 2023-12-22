9206d2080df4f1e28836a291e938e18db5ca6623
Dmitry <dmitrijs.cesnokovs@gmail.com>
2023-12-22 14:53:46 +0200

    DateHelper.getMinutes implementation

--- BerlinClock/BerlinClock/Helpers/DateHelper.swift
+++ BerlinClock/BerlinClock/Helpers/DateHelper.swift
@@ -3,18 +3,23 @@ import Foundation
 struct DateHelper {
     
     static private let calendar = Calendar.current
-
-    static func getSeconds(from date: Date) -> Int {
-        calendar.component(.second, from: date)
-    }
-    
-    static func getHours(from date: Date) -> Int {
-        calendar.component(.hour, from: date)
-    }
     
     static let timeFormat: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "HH:mm:ss"
         return formatter
     }()
+    
+    static func getHours(from date: Date) -> Int {
+        calendar.component(.hour, from: date)
+    }
+    
+    static func getMinutes(from date: Date) -> Int {
+        calendar.component(.minute, from: date)
+    }
+    
+    static func getSeconds(from date: Date) -> Int {
+        calendar.component(.second, from: date)
+    }
+
 }
