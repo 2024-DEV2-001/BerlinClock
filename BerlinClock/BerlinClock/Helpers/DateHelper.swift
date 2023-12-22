f04388a6193af24c68178ba995d539fb28bfd6f4
Dmitry <dmitrijs.cesnokovs@gmail.com>
2023-12-22 14:52:38 +0200

    DateHelper.getHours implementation

--- BerlinClock/BerlinClock/Helpers/DateHelper.swift
+++ BerlinClock/BerlinClock/Helpers/DateHelper.swift
@@ -8,6 +8,10 @@ struct DateHelper {
         calendar.component(.second, from: date)
     }
     
+    static func getHours(from date: Date) -> Int {
+        calendar.component(.hour, from: date)
+    }
+    
     static let timeFormat: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "HH:mm:ss"
