8af0a9908773db00e260a195ddad66618c198693
Dmitry <dmitrijs.cesnokovs@gmail.com>
2023-12-22 16:18:37 +0200

    BerlinViewModel implementation

--- BerlinClock/BerlinClock/View/BerlinClockViewModel.swift
+++ BerlinClock/BerlinClock/View/BerlinClockViewModel.swift
@@ -2,7 +2,14 @@ import Foundation
 import Combine
 
 class BerlinClockViewModel: ObservableObject {
-
+    
+    @Published var timeString = ""
+    @Published var secondsBulb = Bulb.off
+    @Published var fiveHoursRow = [Bulb](repeating: .off, count: 4)
+    @Published var singleHoursRow = [Bulb](repeating: .off, count: 4)
+    @Published var fiveMinutesRow = [Bulb](repeating: .off, count: 11)
+    @Published var singleMinutesRow = [Bulb](repeating: .off, count: 4)
+    
     private var tickerService: TickerService
     private var berlinClock: BerlinClockProtocol
     private var cancellables = Set<AnyCancellable>()
@@ -10,6 +17,20 @@ class BerlinClockViewModel: ObservableObject {
     init(tickerService: TickerService, berlinClock: BerlinClockProtocol) {
         self.berlinClock = berlinClock
         self.tickerService = tickerService
+        tickerService.start()
+        tickerService
+            .$datePublisher
+            .sink { [weak self] date in
+                guard var berlinClock = self?.berlinClock else { return }
+                berlinClock.date = date
+                self?.secondsBulb = berlinClock.getSecondsBulb()
+                self?.fiveHoursRow = berlinClock.getFiveHourRow()
+                self?.singleHoursRow = berlinClock.getSingleHourRow()
+                self?.fiveMinutesRow = berlinClock.getFiveMinuteRow()
+                self?.singleMinutesRow = berlinClock.getSingleMinuteRow()
+                self?.timeString = berlinClock.getTimeString()
+            }
+            .store(in: &cancellables)
     }
     
     deinit {
