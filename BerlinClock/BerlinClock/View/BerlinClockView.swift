1d854b4592a5432aa70859b74e13cd98df9e4afc
Dmitry <dmitrijs.cesnokovs@gmail.com>
2023-12-22 16:31:00 +0200

    UI

--- BerlinClock/BerlinClock/View/BerlinClockView.swift
+++ BerlinClock/BerlinClock/View/BerlinClockView.swift
@@ -1,17 +1,63 @@
 import SwiftUI
 
 struct BerlinClockView: View {
+    @StateObject private var viewModel = BerlinClockViewModelFactory.createBerlinClockViewModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            RoundBulbView(light: $viewModel.secondsBulb)
+                .frame(width: 75, height: 75)
+            BulbRowView(bulbs: $viewModel.fiveHoursRow)
+            BulbRowView(bulbs: $viewModel.singleHoursRow)
+            BulbRowView(bulbs: $viewModel.fiveMinutesRow)
+            BulbRowView(bulbs: $viewModel.singleMinutesRow)
+            Text(viewModel.timeString)
+                .padding()
         }
-        .padding()
+        .padding(25)
     }
 }
 
-#Preview {
-    BerlinClockView()
+struct BulbRowView: View {
+    @Binding var bulbs: [Bulb]
+    
+    var body: some View {
+        HStack {
+            ForEach($bulbs, id: \.id) { $bulb in
+                RectangleBulbView(light: $bulb)
+                    .padding(2)
+                    .frame(minWidth: 0, maxWidth: .infinity)
+            }
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .frame(height: 50)
+        }
+    }
+}
+
+struct RectangleBulbView: View {
+    @Binding var light: Bulb
+    
+    var body: some View {
+        Rectangle()
+            .strokeBorder(.black, lineWidth: 2)
+            .background(Rectangle().fill(light.color))
+    }
+}
+
+struct RoundBulbView: View {
+    @Binding var light: Bulb
+    
+    var body: some View {
+        Circle()
+            .strokeBorder(.black, lineWidth: 2)
+            .background(Circle().fill(light.color))
+    }
+}
+
+// MARK: - Preview
+
+struct ClockView_Previews: PreviewProvider {
+    static var previews: some View {
+        BerlinClockView()
+    }
 }
