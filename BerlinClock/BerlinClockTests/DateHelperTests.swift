39bfb487f8d6ab96841ca6a5e99819132abd2ba4
Dmitry <dmitrijs.cesnokovs@gmail.com>
2023-12-22 15:03:19 +0200

    DateHelper.getNextSecondFromNow test Refactor

--- BerlinClock/BerlinClockTests/DateHelperTests.swift
+++ BerlinClock/BerlinClockTests/DateHelperTests.swift
@@ -4,9 +4,10 @@ final class DateHelperTests: XCTestCase {
     
     let testDateInSeconds:Double = 1000000000 //04:46:40
     
-    func testGetSecondsFromDate() {
+    func testTimeFormat() {
         let date = Date(timeIntervalSince1970: testDateInSeconds)
-        XCTAssertEqual(DateHelper.getSeconds(from: date), 40)
+        let formattedString = DateHelper.timeFormat.string(from: date)
+        XCTAssertEqual(formattedString, "04:46:40")
     }
     
     func testGetHoursFromDate() {
@@ -19,9 +20,20 @@ final class DateHelperTests: XCTestCase {
         XCTAssertEqual(DateHelper.getMinutes(from: date), 46)
     }
     
-    func testTimeFormat() {
+    func testGetSecondsFromDate() {
         let date = Date(timeIntervalSince1970: testDateInSeconds)
-        let formattedString = DateHelper.timeFormat.string(from: date)
-        XCTAssertEqual(formattedString, "04:46:40")
+        XCTAssertEqual(DateHelper.getSeconds(from: date), 40)
+    }
+    
+    func testGetNextSecondFromNow() {
+        let currentDate = Date()
+        let nextSecondDate = DateHelper.getNextSecondFromNow()
+        
+        XCTAssertNotNil(nextSecondDate)
+        
+        if let nextSecondDate = nextSecondDate {
+            let components = Calendar.current.dateComponents([.second], from: currentDate, to: nextSecondDate)
+            XCTAssertEqual(components.second, 1)
+        }
     }
 }
